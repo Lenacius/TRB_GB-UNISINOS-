9edecf6066a0f35a048daa2914a326f01ebe3b7d
Lenacius <39158258+Lenacius@users.noreply.github.com>
2018-06-07 11:01:55 -0300

    added função listagem_arquivos atualizados

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include "Confeitaria.h"
+#include "C:\Users\lolpa\Desktop\C++Projects\TRB_GB\include\Confeitaria.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
 
-    Confeitaria *sonho = new Confeitaria();
+    Confeitaria *sonho = new Confeitaria(); // QUANDO CRIADA A VARIÁVEL O CONSTRUTOR JÁ PUXA OS DADOS DO ARQUIVO Produtos.txt
 
+    //sonho->listarProdutos(); // FUNÇÃO PARA CRIAR A LISTA DE PRODUTOS (SEM LISTA DE INSUMOS)
 
     delete sonho;
     return 0;
