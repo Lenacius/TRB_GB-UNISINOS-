de3e34281131963b4d9140466644e6902a270165
Lenacius <39158258+Lenacius@users.noreply.github.com>
2018-06-18 21:32:15 -0300

    Lista de vendas funcionando!
    Add construtor e getter&setters

--- src/Venda.cpp
+++ src/Venda.cpp
@@ -1,11 +1,20 @@
-#include "C:\Users\isamurs\Desktop\TRB_GB\include\Venda.h"
+#include "Venda.h"
 
-Venda::Venda()
-{
-    //ctor
+/// CONSTRUTOR
+Venda::Venda(){}
+
+Venda::Venda(Produto *produto, float quantidade){
+
+    this->produto = produto;
+    this->quantidade = quantidade;
+
 }
 
-Venda::~Venda()
-{
-    //dtor
-}
+/// DESTRUTOR
+Venda::~Venda(){}
+
+/// SETTERS_&_GETTERS
+float Venda::getQuantidade(){ return this->quantidade; }
+string Venda::getNomeProduto(){ return this->produto->getNome(); }
+
+void Venda::setQuantidade(float quantidade){ this->quantidade = quantidade; }
