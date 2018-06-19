65e8309999fe9b02d541699aecd4e39da64d4aed
Lenacius <39158258+Lenacius@users.noreply.github.com>
2018-06-18 21:31:37 -0300

    Classe vendas funcionando
    Add construtor e getter&setters

--- include/Venda.h
+++ include/Venda.h
@@ -1,16 +1,37 @@
 #ifndef VENDA_H
 #define VENDA_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <fstream>
 
-class Venda
-{
-    public:
-        Venda();
-        virtual ~Venda();
+#include "Produto.h"
 
-    protected:
+using namespace std;
+
+class Venda{
 
     private:
+        /// ATRIBUTOS
+        Produto *produto;
+        float quantidade;
+
+    public:
+        /// CONSTRUTOR
+        Venda();
+        Venda(Produto *produto, float quantidade);
+
+        /// DESTRUTOR
+        virtual ~Venda();
+
+        /// SETTERS_&_GETTERS
+        float getQuantidade();
+        string getNomeProduto();
+
+        void setQuantidade(float quantidade);
+
 };
 
 #endif // VENDA_H
