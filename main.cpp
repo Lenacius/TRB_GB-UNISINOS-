31cde80ba3da219fb12fe56dbce18ab82b35b337
Lenacius <39158258+Lenacius@users.noreply.github.com>
2018-07-02 10:30:45 -0300

    menu created

--- main.cpp
+++ main.cpp
@@ -1,29 +1,161 @@
-//#include "C:\Users\lolpa\Desktop\C++Projects\TRB_GB\include\Confeitaria.h" //QUANDO FOR TRABALHAR NO TEU PC, COMENTA A LINHA ABAIXO
-//#include "C:\Users\jonas\Documents\GitHub\TRB_GB-UNISINOS-\include\Confeitaria.h"
-#include "C:\Users\jonas\Desktop\TrabalhoGBc1\include\Confeitaria.h"
-
-#include <iostream>
-
-using namespace std;
+#include "Confeitaria.h"
+#include "cstdlib"
+#include "cstdio"
 
 int main(){
 
-    Confeitaria *sonho = new Confeitaria(); // QUANDO CRIADA A VARIÁVEL O CONSTRUTOR JÁ PUXA OS DADOS DO ARQUIVO Produtos.txt
+    Confeitaria *sonho = new Confeitaria();
 
-    sonho->listarProdutos(); // FUNÇÃO PARA CRIAR A LISTA DE PRODUTOS (SEM LISTA DE INSUMOS DO PRODUTO)
-//    sonho->listarInsumos(); // FUNÇÃO PARA CRIAR A LISTA DE INSUMOS
+    char opcao = ' ';
 
-//    sonho->consultaProduto(2);
-//    sonho->consultaInsumo(3);
+    /*sonho->listarProdutos();
+    //sonho->listarInsumos();
 
-//    sonho->atualizarInsumos();
-//    cout << endl;
-//    sonho->listarInsumos();
+    //sonho->consultaInsumo(1);
+    //sonho->consultaInsumo("Sal");
 
+    //sonho->atualizarProduto();
+
+    //sonho->consultaProduto(1);
+    sonho->consultaProduto("Pao branco");
+
+    sonho->venderProdutos();
+
+    //sonho->listarProdutos();
+    //sonho->listarVendas();
+
+    //sonho->salvarConfeitaria();
+    sonho->consultaProduto(1);*/
+
+    /*sonho->listarInsumos();
+    sonho->atualizarInsumos();
+    sonho->listarInsumos();*/
+
+    /*sonho->listarProdutos();
+    sonho->listarInsumos();
     sonho->atualizarProdutos();
-    cout << endl;
     sonho->listarProdutos();
+    sonho->listarInsumos();*/
+
+    while(opcao != '0'){
+
+        system("cls");
+
+        cout << "Administrador de Confeitaria" << endl;
+        cout << "1 - Realizar venda" << endl
+        << "2 - Consultar produto" << endl
+        << "3 - Consultar insumo" << endl
+        << "4 - Listar produtos a produzir" << endl
+        << "5 - Listar insumos a comprar" << endl
+        << "6 - Listar todos os produtos" << endl
+        << "7 - Listar todos os insumos" << endl
+        << "8 - Listar vendas" << endl
+        << "9 - Salvar" << endl
+        << "0 - Sair" << endl
+        << "\nOpcao: ";
+
+        cin >> opcao;
+
+        switch(opcao){
+
+            case '1':
+                sonho->venderProdutos();//PROBLEMA
+                break;
+
+            case '2':
+                fflush(stdin);
+                cout << "Digite <1> para consultar por ID - Digite <2> para consultar por Nome: ";
+                cin >> opcao;
+
+                if(opcao == '1'){
+                    int id;
+                    fflush(stdin);
+                    cout << "Digite o ID do produto desejado: ";
+                    cin >> id;
+                    sonho->consultaProduto(id);
+                    break;
+                }
+                else if(opcao == '2'){
+                    string nome;
+                    fflush(stdin);
+                    cout << "Digite o Nome do produto desejado: ";
+                    getline(cin, nome);
+                    sonho->consultaProduto(nome);
+                    break;
+                }
+                else cout << "ERRO! OPCAO INVALIDA!" << endl;
+
+                break;
+
+            case '3':
+                fflush(stdin);
+                cout << "Digite <1> para consultar por ID - Digite <2> para consultar por Nome: ";
+                cin >> opcao;
+
+                if(opcao == '1'){
+                    int id;
+                    fflush(stdin);
+                    cout << "Digite o ID do insumo desejado: ";
+                    cin >> id;
+                    sonho->consultaInsumo(id);
+                    break;
+                }
+                else if(opcao == '2'){
+                    string nome;
+                    fflush(stdin);
+                    cout << "Digite o Nome do insumo desejado: ";
+                    getline(cin, nome);
+                    sonho->consultaInsumo(nome);
+                    break;
+                }
+                else cout << "ERRO! OPCAO INVALIDA!" << endl;
+
+                break;
+
+            case '4':
+                sonho->atualizarProdutos();
+                break;
+
+            case '5':
+                sonho->atualizarInsumos();
+                break;
+
+            case '6':
+                sonho->listarProdutos();
+                break;
+
+            case '7':
+                sonho->listarInsumos();
+                break;
+
+            case '8':
+                sonho->listarVendas();
+                break;
+
+            case '9':
+                cout << "\nSalvando dados do sistema...\n" << endl;
+                sonho->salvarConfeitaria();
+                break;
+
+            case '0':
+                cout << "\ndesligando programa...\n" << endl;
+                sonho->salvarConfeitaria();
+                break;
+
+            case 'p':
+                break;
+
+            default:
+                cout << "\nERRO! OPCAO INVALIDA!\n" << endl;
+                break;
+
+        }
+
+        system("pause");
+
+    }
 
     delete sonho;
     return 0;
+
 }
